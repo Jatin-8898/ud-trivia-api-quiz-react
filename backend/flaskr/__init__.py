fa728017ca9bfcad3c8e2fc7dbb70539b513912e
Jatin Varlyani <varlyanijatin88@gmail.com>
2020-05-15 22:45:00 +0530

    Added Del Question, Search, Post new question, get categories based Q, Play Quiz, Bonus API, Error handlers

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -88,7 +88,20 @@ def create_app(test_config=None):
 	TEST: When you click the trash icon next to a question, the question will be removed.
 	This removal will persist in the database and when you refresh the page. 
 	'''
-
+	@app.route("/questions/<question_id>", methods=['DELETE'])
+	def delete_question(question_id):
+		question = Question.query.filter(Question.id == question_id).one_or_none()
+		if not question:
+			# If no question with given id was found, raise 404 
+			abort(404, {'message': 'Question with id {} does not exist.'.format(question_id)})
+		try:
+			question.delete()
+			return jsonify({
+				'success': True,
+				'deleted': question_id
+			})
+		except:
+			abort(422)
 	'''
 	@TODO: 
 	Create an endpoint to POST a new question, 
@@ -99,7 +112,48 @@ def create_app(test_config=None):
 	the form will clear and the question will appear at the end of the last page
 	of the questions list in the "List" tab.  
 	'''
+	@app.route("/questions", methods=['POST'])
+	def add_question():
+		body = request.get_json()
 
+		question = body.get('question')
+		answer = body.get('answer')
+		category = body.get('category')
+		difficulty = body.get('difficulty')
+
+		# Check all the fiels are present
+		if not question:
+			abort(400, {'message': 'Question can not be blank'})
+
+		if not answer:
+			abort(400, {'message': 'Answer can not be blank'})
+
+		if not category:
+			abort(400, {'message': 'Category can not be blank'})
+
+		if not difficulty:
+			abort(400, {'message': 'Difficulty can not be blank'})
+
+		try:
+			question = Question(
+				question=question, 
+				answer=answer, 
+				difficulty=difficulty, 
+				category=category)
+			question.insert()
+
+			# After succesfully insertion, get all the  paginated questionss 
+			selections = Question.query.order_by(Question.id).all()
+			questions_paginated = paginate_questions(request, selections)
+
+			return jsonify({
+				'success': True,
+				'created': question.id,
+				'questions': questions_paginated,
+        		'total_questions': len(selections)
+			})
+		except:
+			abort(422)
 	'''
 	@TODO: 
 	Create a POST endpoint to get questions based on a search term. 
@@ -110,7 +164,21 @@ def create_app(test_config=None):
 	only question that include that string within their question. 
 	Try using the word "title" to start. 
 	'''
+	@app.route('/questions/search', methods=['POST'])
+	def search_questions():
+		body = request.get_json()
+		search_term = body.get('searchTerm', None)
 
+		if search_term:
+			search_results = Question.query.filter(Question.question.ilike(f'%{search_term}%')).all()
+
+			return jsonify({
+				'success': True,
+				'questions': [question.format() for question in search_results],
+				'total_questions': len(search_results),
+				'current_category': None
+			})
+		abort(404)
 	'''
 	@TODO: 
 	Create a GET endpoint to get questions based on category. 
@@ -119,7 +187,19 @@ def create_app(test_config=None):
 	categories in the left column will cause only questions of that 
 	category to be shown. 
 	'''
+	@app.route('/categories/<int:category_id>/questions', methods=['GET'])
+	def get_questions_by_category(category_id):
+		try:
+			questions = Question.query.filter(Question.category == str(category_id)).all()
 
+			return jsonify({
+				'success': True,
+				'questions': [question.format() for question in questions],
+				'total_questions': len(questions),
+				'current_category': category_id
+			})
+		except:
+			abort(404)
 
 	'''
 	@TODO: 
@@ -132,13 +212,133 @@ def create_app(test_config=None):
 	one question at a time is displayed, the user is allowed to answer
 	and shown whether they were correct or not. 
 	'''
+	@app.route('/quizzes', methods=['POST'])
+	def play_quiz():
+		body = request.get_json()
+		if not body:
+			# If no json body given, raise error.
+			abort(400, {'message': 'Please provide a JSON body with previous Question Ids and optional category. Thanks'})
+		try:
+			
+			if not ('quiz_category' in body and 'previous_questions' in body):
+				abort(422)
 
+			# Get paramters from JSON Body.
+			previous_questions = body.get('previous_questions', None)
+			category = body.get('quiz_category', None)
+
+			if category['type'] == 'click':
+				available_questions = (Question.query
+								.filter(Question.id.notin_((previous_questions)))
+								.all())
+			else:
+				available_questions = (Question.query
+								.filter_by(category=category['id'])
+								.filter(Question.id.notin_((previous_questions)))
+								.all())
+
+			 # Format questions & get a random question
+			questions_formatted = [question.format() for question in available_questions]
+			random_question = questions_formatted[random.randint(0, len(questions_formatted))]
+	
+			return jsonify({
+				'success': True,
+				'question': random_question
+			})
+		except:
+			abort(422)
+	'''
+	BONUS: API to create and delete Categories
+	'''
+	@app.route('/categories', methods=['POST'])
+	def create_categories():
+		body = request.get_json()
+
+		if not body:
+			abort(400, {'message': 'Request does not contain a valid JSON body.'})
+
+		# Get field informations from request body
+		type = body.get('type', None)
+
+		# Check all required fields are given.
+		if not type:
+			abort(400, {'message': 'No type for New category provided.'})
+		try:
+			category = Category(type = type)
+			category.insert()
+
+			# After succesfully insertion, get all categories 
+			selections = Category.query.order_by(Category.id).all()
+			categories_all = [category.format() for category in selections]
+
+			# Return response
+			return jsonify({
+				'success': True,
+				'created': category.id,
+				'categories': categories_all,
+				'total_categories': len(selections)
+			})
+		except:
+			abort(422)
+
+	@app.route('/categories/<int:category_id>', methods=['DELETE'])
+	def delete_categories(category_id):
+		category = Category.query.filter(Category.id == category_id).one_or_none()
+		
+		if not category:
+			# If no category with given id was found, raise 404 and explain what went wrong.
+			abort(400, {'message': 'Category with id {} does not exist.'.format(category_id)})
+		try:
+			category.delete()
+			return jsonify({
+				'success': True,
+				'deleted': category_id
+			})
+		except:
+			abort(422)
 	'''
 	@TODO: 
 	Create error handlers for all expected errors 
 	including 404 and 422. 
 	'''
-	
-	return app
+	@app.errorhandler(400)
+	def bad_request(error):
+		return jsonify({
+			"success": False, 
+			"error": 400,
+			"message": getErrorMessage(error, "bad request")
+		}), 400
 
-		
+	@app.errorhandler(404)
+	def ressource_not_found(error):
+		return jsonify({
+			"success": False, 
+			"error": 404,
+			"message": getErrorMessage(error, "resource not found")
+		}), 404
+
+	@app.errorhandler(405)
+	def method_not_allowed(error):
+		return jsonify({
+			"success": False, 
+			"error": 405,
+			"message": "method not allowed"
+		}), 405
+
+	@app.errorhandler(422)
+	def unprocessable(error):
+		return jsonify({
+			"success": False, 
+			"error": 422,
+			"message": getErrorMessage(error, "unprocessable")
+		}), 422
+	
+	@app.errorhandler(500)
+	def internal_server_error(error):
+		return jsonify({
+			"success": False, 
+			"error": 500,
+			"message": "internal server error"
+		}), 500
+
+	return app
